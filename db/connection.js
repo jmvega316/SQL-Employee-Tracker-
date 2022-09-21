c24fd484eb685da2e86153cf4707d5f0e91572a2
jmvega316 <jmvega316@gmail.com>
2022-09-21 11:18:37 -0400

    added files

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+const mysql2 = require("mysql2")  
+const connection = mysql2.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Gokusolos",
+    database:"inventory_db"
+
+})
+connection.connect(function (err){
+    if(err) throw err
+})
+
+
+module.exports = connection
---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE inventory_db;
+
+CREATE TABLE team (
+    id INT PRIMARY KEY,
+    title VARCHAR(10),
+    Name INT,
+    role VARCHAR(10),
+);
+
+/*DROP is to delete
+
+ 
